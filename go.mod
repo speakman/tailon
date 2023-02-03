1e44848f6fd5f053ec49b286a585c7b5ab5bfdb8
Daniel Nyström <daniel@nystrom.st>
2023-02-03 11:32:59 +0100

    Bump sockjs to v3

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/gvalkov/tailon
+
+go 1.19
+
+require (
+	github.com/gorilla/handlers v1.5.1
+	github.com/mitchellh/mapstructure v1.5.0
+	github.com/pelletier/go-toml v1.9.5
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749
+	github.com/shurcooL/httpgzip v0.0.0-20190720172056-320755c1c1b0
+	github.com/spf13/pflag v1.0.5
+	gopkg.in/igm/sockjs-go.v3 v3.0.2
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/igm/sockjs-go v3.0.2+incompatible // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
+)
--- server.go
+++ server.go
@@ -2,12 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/handlers"
-	"github.com/gvalkov/tailon/cmd"
-	"github.com/gvalkov/tailon/frontend"
-	"github.com/shurcooL/httpfs/html/vfstemplate"
-	"github.com/shurcooL/httpgzip"
-	"gopkg.in/igm/sockjs-go.v2/sockjs"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,6 +9,13 @@ import (
 	"os/exec"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gvalkov/tailon/cmd"
+	"github.com/gvalkov/tailon/frontend"
+	"github.com/shurcooL/httpfs/html/vfstemplate"
+	"github.com/shurcooL/httpgzip"
+	"gopkg.in/igm/sockjs-go.v3/sockjs"
 )
 
 func setupRoutes(relativeroot string) *http.ServeMux {
@@ -162,7 +163,8 @@ func wsWriter(session sockjs.Session, messages chan string, done <-chan struct{}
 
 // Expands the variables in main.CommandSpec.Action with the values in the
 // frontend command. For example:
-//    ["tail", "-n", "$lines", "-F", "$path"] -> ["tail", "-n", "10", "-F", "f1.txt"]
+//
+//	["tail", "-n", "$lines", "-F", "$path"] -> ["tail", "-n", "10", "-F", "f1.txt"]
 func expandCommandArgs(action []string, cmd FrontendCommand) []string {
 	var res = make([]string, 0)
 
